a642abf8e2a52dfc256f55a90adc0dc2feb14a2c
Lavish Patidar <lavishpatidar9926@gmail.com>
2025-02-27 12:00:41 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import cors from 'cors';
+import typeDefs from './schemas/quizSchema.js'; // Correct import path
+
+import resolvers from './resolvers/quizResolvers.js'; // Correct import path
+import connectDB from './config/db.js'; // Ensure to add .js extension
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const serverStart = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    const port = process.env.PORT || 4000;
+
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+};
+
+connectDB(); // Ensure the database connection is established before starting the server
+serverStart();
